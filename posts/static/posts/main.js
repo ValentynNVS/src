eccd8cc6a863cfeb487fcb21a2918bd7ece2aac1
Valentyn Novosydliuk <novosidlak3@gmail.com>
2025-04-06 01:44:45 -0400

    first ajax call done

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -2,4 +2,16 @@ console.log("hello world")
 
 const helloWorlBox = document.getElementById('hello-world')
 
-helloWorlBox.textContent = 'hello world'
+
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response) {
+        console.log('success', response.text)
+        helloWorlBox.textContent = response.text
+    },
+    error: function(error) {
+        console.log('error', error)
+    }
+})
