7fdf21c82cacb90128390a8c824018352d57ac01
Valentyn Novosydliuk <novosidlak3@gmail.com>
2025-04-07 12:17:38 -0400

    Added a Form to the Modal

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from .forms import PostForm
+from profiles.models import Profile
 # from django.core import serializers
 # Create your views here.
 
 def post_list_and_create(request):
+    form = PostForm(request.POST or None)
     qs = Post.objects.all()
-    return render(request, 'posts/main.html', {'qs':qs} )
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        if form.is_valid():
+            author = Profile.objects.get(user=request.user)
+            instance = form.save(commit=False)
+            instance.author = author
+            instance.save()
+    context = {
+        'form' : form,
+    }
+
+    return render(request, 'posts/main.html', context)
 
 def load_post_data_view(request, num_posts):
     # qs = Post.objects.all()
