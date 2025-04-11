63dcf7773f1ce4667c5ccd409602ed7c9c886b84
Valentyn Novosydliuk <novosidlak3@gmail.com>
2025-04-10 22:08:46 -0400

    the project is done, but i will start adding my modifications

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Photo
 from django.http import JsonResponse, HttpResponse
 from .forms import PostForm
 from profiles.models import Profile
 from .utils import action_permission
 from django.contrib.auth.decorators import login_required
-# from django.core import serializers
-# Create your views here.
 
 @login_required
 def post_list_and_create(request):
@@ -20,16 +18,18 @@ def post_list_and_create(request):
             instance.author = author
             instance.save()
             return JsonResponse({
-                'title':instance.title,
+                'title': instance.title,
                 'body': instance.body,
                 'author': instance.author.user.username,
                 'id': instance.id,
             })
-    context = {
-        'form' : form,
-    }
+        context = {
+            'form': form,
+        }
+        return render(request, 'posts/main.html', context)
 
-    return render(request, 'posts/main.html', context)
+    return render(request, 'posts/main.html', {'form': form})
+
 
 @login_required
 def post_detail(request, pk):
@@ -40,20 +40,23 @@ def post_detail(request, pk):
         'obj': obj,
         'form': form,
     }
+
+    # If AJAX, return partial render
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'posts/detail.html', context)
     
-    return render(request, 'posts/detail.html',context)
+    # Otherwise, render full page
+    return render(request, 'posts/detail.html', context)
+
 
 @login_required
 def load_post_data_view(request, num_posts):
-    # qs = Post.objects.all()
-    # return JsonResponse({'qs':qs})
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         visible = 3
         upper = num_posts
         lower = upper - visible
         size = Post.objects.all().count()
         qs = Post.objects.all()
-        # data = serializers.serialize('json', qs)
         data = []
         for obj in qs:
             item = {
@@ -66,19 +69,21 @@ def load_post_data_view(request, num_posts):
             }
             data.append(item)
         return JsonResponse({'data':data[lower:upper], 'size':size})
+    return redirect('posts:main-board')
     
 @login_required
 def post_detail_data_view(request,pk):
-    obj = Post.objects.get(pk=pk)
-    data  = {
-        'id': obj.id,
-        'title':obj.title,
-        'body':obj.body,
-        'author':obj.author.user.username,
-        'logged_in': request.user.username,
-    }
-    return JsonResponse({'data':data})
-
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        obj = Post.objects.get(pk=pk)
+        data  = {
+            'id': obj.id,
+            'title':obj.title,
+            'body':obj.body,
+            'author':obj.author.user.username,
+            'logged_in': request.user.username,
+        }
+        return JsonResponse({'data':data})
+    return redirect('posts:main-board')
 
 @login_required
 def like_unlike_post(request):
@@ -92,6 +97,7 @@ def like_unlike_post(request):
             liked = True
             obj.liked.add(request.user)
         return JsonResponse({'liked':liked, 'count':obj.like_count})
+    return redirect('posts:main-board')
 
 @login_required 
 @action_permission
@@ -107,6 +113,7 @@ def update_post(request,pk):
             'title':new_title,
             'body':new_body,
         })
+    return redirect('posts:main-board')
 
 @login_required
 @action_permission
@@ -115,7 +122,7 @@ def delete_post(request,pk):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         obj.delete()
         return JsonResponse({'msg':'some message'})
-    return JsonResponse({'msg': 'access denied - ajax only'})
+    return redirect('posts:main-board')
 
 @login_required
 def image_upload_view(request):
